4b56a182b57c7b2e443f383b71519d2cfa4cd9d1
MattAllman94 <62230366+MattAllman94@users.noreply.github.com>
2021-04-27 11:13:00 +1000

    Door Animations

---
+++ Assets/Scripts/SlidingDoor.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SlidingDoor : MonoBehaviour
+{
+    Animator anim;
+    public bool playerPresent = false;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (playerPresent)
+        {
+            anim.SetBool("Open", true);
+            anim.SetBool("Close", false);
+        }
+
+        else if(!playerPresent)
+        {
+            anim.SetBool("Close", true);
+            anim.SetBool("Open", false);
+        }    
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player"))
+        {
+            playerPresent = true;
+        }
+        else
+        {
+            playerPresent = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            playerPresent = false;
+        }
+        else
+        {
+            playerPresent = true;
+        }
+    }
+}
